ce1a851b8d1765fb2be58ec10e0f7b1c427479bf
George Voronin <bordernormal@gmail.com>
2023-12-09 01:27:28 +0700

    Create basic classes

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] Args) {
+
+    }
+    public static Javalin getApp() {
+        return null;
+    }
+}
