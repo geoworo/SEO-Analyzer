6f63292d3f0e1fe1b77b5355c32f9800a663b993
George Voronin <bordernormal@gmail.com>
2023-12-10 03:23:50 +0700

    Set up main class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,11 +2,30 @@ package hexlet.code;
 
 import io.javalin.Javalin;
 
-public class App {
-    public static void main(String[] Args) {
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class App {
+    public static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
     }
-    public static Javalin getApp() {
-        return null;
+    public static void main(String[] Args) throws SQLException, IOException {
+        var app = getApp();
+        app.start(getPort());
+    }
+    public static Javalin getApp() throws IOException, SQLException {
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> {
+            ctx.result("Hello World");
+        });
+
+        return app;
     }
 }
