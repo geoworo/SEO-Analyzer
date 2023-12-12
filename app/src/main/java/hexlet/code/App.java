342620502b4126260e7fb91f642f45f7110a49b9
George Voronin <bordernormal@gmail.com>
2023-12-13 01:12:31 +0700

    Add DB

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,14 @@
 package hexlet.code;
 
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 
 public class App {
@@ -13,11 +16,30 @@ public class App {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.parseInt(port);
     }
+
+    public static String readResourceFile(String filename) throws IOException {
+        var path = Paths.get("src", "main", "resources", filename);
+        return Files.readString(path);
+    }
+
     public static void main(String[] Args) throws SQLException, IOException {
         var app = getApp();
         app.start(getPort());
     }
+
     public static Javalin getApp() throws IOException, SQLException {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var sql = readResourceFile("schema.sql");
+
+        try (var connection = dataSource.getConnection();
+                var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+        
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,21 @@
+package hexlet.code.model;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Url {
+    private Long id;
+    private String name;
+    private Timestamp createdAt;
+
+    public Url(String name, Timestamp createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+}
