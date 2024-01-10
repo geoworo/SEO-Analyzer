bcb41d6f31b1dac06b0a6f98b88bbc2380ccf742
George Voronin <bordernormal@gmail.com>
2024-01-11 06:59:30 +0700

    Fix sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -13,6 +13,6 @@ CREATE TABLE url_checks (
     status_code INTEGER NOT NULL,
     h1 VARCHAR(255),
     title VARCHAR(255),
-    description VARCHAR(MAX),
+    description TEXT,
     created_at TIMESTAMP NOT NULL
 );
